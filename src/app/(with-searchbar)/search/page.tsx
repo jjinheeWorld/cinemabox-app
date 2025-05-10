f1809cd2408be8de22916ce219cc0165046d1ff3
jjinheeWorld <wlsgml450@naver.com>
2025-05-10 19:15:42 +0900

    style: 검색 페이지 UI 구현하기

--- src/app/(with-searchbar)/search/page.tsx
+++ src/app/(with-searchbar)/search/page.tsx
@@ -1,8 +1,18 @@
+import MovieItem from "@/component/movie-item";
+import movies from "@/dummy.json";
+import style from "./page.module.css";
+
 export default async function Page({
   searchParams,
 }: {
   searchParams: Promise<{ q: string }>;
 }) {
   const { q } = await searchParams;
-  return <div>Search : {q}</div>;
+  return (
+    <div className={style.container}>
+      {movies.map((movie) => (
+        <MovieItem key={movie.id} {...movie} />
+      ))}
+    </div>
+  );
 }
