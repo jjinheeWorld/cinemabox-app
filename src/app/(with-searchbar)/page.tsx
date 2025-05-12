c02380e76626ad7169cf9f0b9e8e49e0b2a95da3
jjinheeWorld <wlsgml450@naver.com>
2025-05-12 23:33:10 +0900

    feat: 인덱스 페이지 데이터 캐시 옵션 추가

--- src/app/(with-searchbar)/page.tsx
+++ src/app/(with-searchbar)/page.tsx
@@ -1,25 +1,57 @@
 import MovieItem from "@/component/movie-item";
 import style from "./page.module.css";
-import movies from "@/dummy.json";
+import { MovieData } from "@/types";
+
+async function AllMovies() {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/movie`,
+    { cache: "force-cache" }
+  );
+
+  if (!response.ok) {
+    return <div>오류가 발생했습니다!</div>;
+  }
+
+  const movies: MovieData[] = await response.json();
+  return (
+    <div className={style.all}>
+      {movies.map((movie) => (
+        <MovieItem key={`all-${movie.id}`} {...movie} />
+      ))}
+    </div>
+  );
+}
+
+async function RecoMovies() {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/movie/random`,
+    { next: { revalidate: 3 } }
+  );
+
+  if (!response.ok) {
+    return <div>오류가 발생했습니다!</div>;
+  }
+
+  const movies: MovieData[] = await response.json();
+  return (
+    <div className={style.recommend}>
+      {movies.map((movie) => (
+        <MovieItem key={`reco-${movie.id}`} {...movie} />
+      ))}
+    </div>
+  );
+}
 
 export default function Home() {
   return (
     <div className={style.container}>
       <section>
         <h3>지금 가장 추천하는 영화</h3>
-        <div className={style.recommend}>
-          {movies.slice(0, 3).map((movie) => (
-            <MovieItem key={`reco-${movie.id}`} {...movie} />
-          ))}
-        </div>
+        <RecoMovies />
       </section>
       <section>
         <h3>등록된 모든 영화</h3>
-        <div className={style.all}>
-          {movies.map((movie) => (
-            <MovieItem key={`all-${movie.id}`} {...movie} />
-          ))}
-        </div>
+        <AllMovies />
       </section>
     </div>
   );
