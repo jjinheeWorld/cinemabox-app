d8269bddc50e94e86ed6f775695f6507b02c4468
jjinheeWorld <wlsgml450@naver.com>
2025-05-13 19:33:33 +0900

    fix: Suspense로 쿼리스트링 관련 빌드 오류 해결

--- src/app/(with-searchbar)/layout.tsx
+++ src/app/(with-searchbar)/layout.tsx
@@ -1,10 +1,13 @@
-import { ReactNode } from "react";
+import { ReactNode, Suspense } from "react";
 import SearchBar from "../../component/searchbar";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div>
-      <SearchBar />
+      Suspense
+      <Suspense fallback={<div>Loading...</div>}>
+        <SearchBar />
+      </Suspense>
       {children}
     </div>
   );
