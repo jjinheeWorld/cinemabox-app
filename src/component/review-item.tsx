dec9a653dcf8338a0c295a188ea7b7ab55c930e3
jjinheeWorld <wlsgml450@naver.com>
2025-05-25 23:39:04 +0900

    style: 리뷰 조회 UI 구현하기

--- src/component/review-item.tsx
+++ src/component/review-item.tsx
@@ -1,18 +1,14 @@
 import { ReviewData } from "@/types";
 import style from "./review-item.module.css";
 
-export default function ReviewItem({
-  id,
-  content,
-  author,
-  createdAt,
-  movieId,
-}: ReviewData) {
+export default function ReviewItem({ content, author, createdAt }: ReviewData) {
   return (
     <div className={style.container}>
-      <div className={style.author}>{author}</div>
-      <div className={style.date}>
-        {new Date(createdAt).toLocaleDateString()}일 작성됨
+      <div className={style.top_container}>
+        <div className={style.author}>{author}</div>
+        <div className={style.date}>
+          {new Date(createdAt).toLocaleDateString()}일 작성됨
+        </div>
       </div>
       <div className={style.content}>{content}</div>
       <div className={style.delete_btn}>리뷰 삭제하기</div>
