202b48cb4106422fb39737ba0655657934523e71
jjinheeWorld <wlsgml450@naver.com>
2025-05-27 00:03:44 +0900

    feat: useActionState로 리뷰 추가 기능 개선 - 로딩 UI 및 중복 제출 방지

--- src/actions/create-review.action.ts
+++ src/actions/create-review.action.ts
@@ -2,12 +2,17 @@
 
 import { revalidatePath } from "next/cache";
 
-export async function createReviewAction(formData: FormData) {
+export async function createReviewAction(_: any, formData: FormData) {
   const movieId = formData.get("movieId")?.toString();
   const content = formData.get("content")?.toString();
   const author = formData.get("author")?.toString();
 
-  if (!content || !author) return;
+  if (!movieId || !content || !author) {
+    return {
+      status: false,
+      error: "리뷰 내용과 작성자를 입력해주세요.",
+    };
+  }
 
   try {
     const response = await fetch(
@@ -17,9 +22,21 @@ export async function createReviewAction(formData: FormData) {
         body: JSON.stringify({ movieId, content, author }),
       }
     );
+
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+
     revalidatePath(`review-${movieId}`);
+    return {
+      status: true,
+      error: "",
+    };
   } catch (err) {
     console.log(err);
-    return;
+    return {
+      status: false,
+      error: `리뷰 저장에 실패했습니다. : ${err}`,
+    };
   }
 }
